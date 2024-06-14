d81ec1683547e1a425c66260b2a1ad57ebe15ae5
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-06-14 21:13:42 +0500

    Added Palindrome Problem

---
+++ palindrome.tsx
@@ -0,0 +1,13 @@
+function isPalindrome(x: number): boolean {
+    let y = x.toString()
+    let z = y.split('',)
+    let reverse = z.reverse();
+    let joined = reverse.join('')
+    let num = Number(joined)
+
+    if (x === num){
+        return true
+    }
+    return false;
+};
+console.log(isPalindrome(121))
