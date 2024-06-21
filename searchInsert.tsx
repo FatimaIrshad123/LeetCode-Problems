683b09466e92f1d2aa95273729cdb7994bf76938
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-06-21 20:56:40 +0500

    Added searchInsert.tsx

---
+++ searchInsert.tsx
@@ -0,0 +1,12 @@
+function searchInsert(nums: number[], target: number): any {
+    if (nums.includes(target)){
+        return nums.indexOf(target);
+    }else {
+        nums.push(target);
+        let a = nums.sort((a,b) => a-b);
+        console.log(a)
+        return a.indexOf(target);
+    }
+};
+let res = searchInsert([3,5,7,9,10],8);
+console.log(res);
