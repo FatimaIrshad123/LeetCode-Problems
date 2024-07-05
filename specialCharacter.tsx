ac86e3d448e91b4a5605aa16cd255ee5a160f906
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-07-05 23:45:35 +0500

    Added specialCharacter.tsx

---
+++ specialCharacter.tsx
@@ -0,0 +1,18 @@
+function numberOfSpecialChars(word: string): any {
+    let newWord = new Set();
+    let checked = new Set();
+    let ans = 0;
+
+    for (let i = 0; i<word.length; i++){
+        newWord.add(word[i]);
+        if (newWord.has(word[i].toLowerCase()) && newWord.has(word[i].toUpperCase())){
+            if (!checked.has(word[i].toLowerCase())){
+                ans ++;
+                //console.log(ans);
+            }
+            checked.add(word[i].toLowerCase())
+        }
+    }
+    
+};
+numberOfSpecialChars('abcdeAB')
