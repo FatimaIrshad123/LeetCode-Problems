a44422afd856e9b4468d79ccc13f3a201b0bc955
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-07-11 22:43:36 +0500

    Updated code

--- src/addTwoNumbers.js
+++ src/addTwoNumbers.js
@@ -3,9 +3,14 @@ function addTwoNumbers(l1, l2) {
     let sum = [];
     for (let i = 0; i < l1.length; i++) {
         //console.log(l1[i])
-        sum.push(l1[i] + l2[i]);
+        for (let j = 0; j < l2.length; j++) {
+            //console.log(l2[j]);
+            let x = l1[i] + l2[j];
+            console.log(x);
+        }
+        //sum.push(l1[i] + l2[i])
     }
-    return sum;
+    //return sum;
 }
 ;
 addTwoNumbers([2, 3, 4], [1, 2, 3]);
