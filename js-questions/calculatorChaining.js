52ff05da39a2e9f7855a440b899c3735d6ea549b
FatimaIrshad123 <bibimemoona2017@gmail.com>
2025-01-23 19:57:35 +0500

    Updated code

---
+++ js-questions/calculatorChaining.js
@@ -0,0 +1,36 @@
+class Calculator {
+    constructor(value) {
+        this.value = value;
+        this.error = null;
+    }
+    add(value){
+        this.value += value;
+        return this;
+    }
+    subtract(value){
+        this.value -= value;
+        return this;
+    }
+    multiply(value) {
+        this.value *= value;
+        return this;
+    }
+    divide(value) {
+        if (value == 0){
+            this.error = "Division by zero is not allowed"
+        }else {
+            this.value /= value;
+        }
+        return this;
+    }
+    power(value) {
+        this.value = Math.pow(this.value,value);
+        return this;
+    }
+    getResult() {
+        if (this.error){
+            return this.error;
+        }
+        return this.value;       
+    }
+}
--- problem/dist/singleNumber.js
+++ problem/dist/singleNumber.js
@@ -1,16 +1,11 @@
 "use strict";
 function singleNumber(nums) {
-    let num = [];
-    for (let i = 0; i < nums.length; i++) {
-        //console.log(nums[i])
-        if (nums[i] == nums[i++]) {
-            num.push(nums[i]);
-            console.log(num);
-        }
-    }
-    console.log(num);
     return nums.reduce((prev, val) => prev ^ val, 0);
 }
 ;
 let ab = singleNumber([4, 1, 2, 1, 2]);
 console.log(ab);
+const arr = [10, 20, 25, 100, 40];
+console.log(arr.sort());
+arr.sort((a, b) => a - b);
+console.log(arr);
